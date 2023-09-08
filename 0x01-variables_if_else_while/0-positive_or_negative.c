30097266e6b7af59305808bc2853c5eabcb9238a
DantheSE <dan.doyin@gmail.com>
2023-09-08 13:29:24 -0700

    added documentation for main function

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <time.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/* main checks if a number is positive, negative or zero */
 int main(void)
 {
 	int n;
@@ -12,7 +12,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-	printf("%d is greater than 0\n", n);
+	printf("%d is positive\n", n);
 	}
 	else if	(n == 0)
 	{
