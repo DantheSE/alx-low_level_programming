afa91d2f3a98dba7058d4f1c78dbe25efae85bda
DantheSE <dan.doyin@gmail.com>
2023-09-10 15:00:41 -0700

    finished all mandatory tasks

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints single digit numbers of base 10 followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int digit = 0;
+while (digit < 10)
+{
+putchar(digit + '0');
+digit++;
+}
+putchar('\n');
+return (0);
+}
+
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints lowercase hexadecimal numbers followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int digit;
+for (digit = 0; digit < 10; digit++)
+putchar(digit + '0');
+for (digit = 'a'; digit <= 'f'; digit++)
+putchar(digit);
+putchar('\n');
+return (0);
+}
+
