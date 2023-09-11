2576ba804a7d5c882375260a41e4ff6c2cc76d61
DantheSE <dan.doyin@gmail.com>
2023-09-11 07:18:38 -0700

    fixed error in tasj 4

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,15 +5,19 @@
 /**
  * main - Entry point of the program
  *
+ * Description: Prints the specified message to the standard error.
+ *
  * Return: Always 1 (error code)
  */
 int main(void)
 {
-char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 ssize_t len = 59;
 if (write(2, message, len) != len)
 {
+perror("Write error");
 return (1);
 }
 return (1);
 }
+
