075db4033de5b53321a0c3379ae2fd543f57767d
DantheSE <dan.doyin@gmail.com>
2023-09-11 16:04:21 -0700

    fixed error in last advanced task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
 #include <unistd.h>
+
 /**
  * main - Entry point
- * Description: Prints a message to stderr and returns 1.
- * Return: 1
+ *
+ * Description: Prints the specified message to stderr and returns 1.
+ *
+ * Return: Always 1
  */
 int main(void)
 {
-write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 61);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
 return (1);
 }
