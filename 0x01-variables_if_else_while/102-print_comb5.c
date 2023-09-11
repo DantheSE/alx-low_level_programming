8a788c07556cdd1747f8fb8dc9661795a9783ad2
DantheSE <dan.doyin@gmail.com>
2023-09-11 16:47:35 -0700

    hoy fix 5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,15 +9,15 @@
 int main(void)
 {
 int i, j;
-for (i = 0; i < 100; i++)
+	for (i = 0; i < 100; i++)
 {
-for (j = i + 1; j < 100; j++)
+		for (j = i + 1; j < 100; j++)
 {
-printf("%02d %02d", i, j);
-	if (i != 98 || j != 99)
+			printf("%02d %02d", i, j);
+			if (i != 98 || j != 99)
 {
-putchar(',');
-putchar(' ');
+				putchar(',');
+				putchar(' ');
 }
 }
 }
