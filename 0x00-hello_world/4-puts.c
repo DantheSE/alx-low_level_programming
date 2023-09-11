61a6a9c0245826d95d4460418b72464efa562484
DantheSE <dan.doyin@gmail.com>
2023-09-11 08:04:08 -0700

    fixed task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,6 @@
  */
 int main(void)
 {
-putchar('\t'); /* Display a tab (indentation) */
 putchar('"');  /* Display the opening double quotation mark */
 puts("Programming is like building a multilingual puzzle");
 return (0);
