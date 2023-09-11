bd4607e2e867ea3c446cd85bc8542361dcf7d740
DantheSE <dan.doyin@gmail.com>
2023-09-11 16:44:12 -0700

    hot fix 4

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,9 +14,9 @@ for (i = 0; i < 100; i++)
 for (j = i + 1; j < 100; j++)
 {
 printf("%02d %02d", i, j);
-if (i != 98 || j != 99)
+	if (i != 98 || j != 99)
 {
- putchar(',');
+putchar(',');
 putchar(' ');
 }
 }
