12510f0ac8495295c588b7b4e315e8f92aa2109d
DantheSE <dan.doyin@gmail.com>
2023-09-11 16:26:53 -0700

    hot fix 1

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,32 +1,30 @@
 #include <stdio.h>
 
 /**
- * main - Prints two-digit number pairs in ascending order.
+ * main - Prints all possible combinations of two two-digit numbers.
  *
- * Return: 0 for success.
+ * Description: Combinations range from 00 to 99, separated by a comma.
+ * Return: Always 0
  */
 int main(void)
 {
-	int i, j;
-
-	for (i = 0; i <= 99; i++)
-	{
-		for (j = i; j <= 99; j++)
-		{
-			putchar((i / 10) + '0');
-			putchar((i % 10) + '0');
-			putchar(' ');
-			putchar((j / 10) + '0');
-			putchar((j % 10) + '0');
-
-			if (i != 99 || j != 99)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
-	}
-
-	return (0);
+int i, j;
+for (i = 0; i < 100; i++)
+{
+for (j = i; j < 100; j++)
+{
+putchar((i / 10) + '0');
+putchar((i % 10) + '0');
+putchar(' ');
+putchar((j / 10) + '0');
+putchar((j % 10) + '0');
+if (i != 99 || j != 99)
+{
+putchar(',');
+putchar(' ');
 }
-
+}
+}
+putchar('\n');
+return (0);
+}
