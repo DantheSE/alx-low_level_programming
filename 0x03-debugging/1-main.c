c7d3c2d1e2a64832d24ef401e7ea6dd2d36a4b16
DantheSE <dan.doyin@gmail.com>
2023-09-14 13:04:50 -0700

    fixed errors

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -15,7 +15,7 @@ int main(void)
 
     while (i < 10)
     {
-        /* putchar(i); */  // Commented out to avoid the infinite loop
+/*         putchar(i); */
     }
 
     printf("Infinite loop avoided! \\o/\n");
