2e836d760ba74cf54c2d522d53d57bb2e2cb03bd
DantheSE <dan.doyin@gmail.com>
2023-09-14 09:04:54 -0700

    first iteration for task 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - prints all the letters of the alphabet by calling a function
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+print_alphabet();
+return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+/**
+ * print_alphabet - function that loops effectively printing the full alphabet
+ */
 int _putchar(char c);
-
+void print_alphabet(void)
+{
+for (print_alphabet = 'a'; print_alphabet <= 'z'; print_alphabet++)
+{
+	printf("%c", print_alphabet);
+}
+}
 #endif /* MAIN_H */
