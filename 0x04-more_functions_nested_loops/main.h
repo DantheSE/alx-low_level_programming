64592e39665c436897645a4bd73c595a11ceba8d
DantheSE <dan.doyin@gmail.com>
2023-09-15 09:19:02 -0700

    solution to task 1

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,8 +4,10 @@
 /**
  *_isupper - confirms presecnce of capital letetrs
  *
+ * @c: the character to check
  * Return: 0 or 1
  */
+
 
 int _isupper(int c)
 {
@@ -16,6 +18,25 @@ int _isupper(int c)
 	else
 	{
 		return (0);
+	}
+}
+/**
+ * _isdigit - checks if a character is a digit between 0 and 9
+ * @c: the character to check
+ *
+ * Return:
+ * 1 if the character is a digit between 0 and 9, 0 otherwise.
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
 	}
 }
 #endif /*MAIN_H*/
