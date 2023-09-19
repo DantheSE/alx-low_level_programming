4b0750ecb64c86f8fdc63a15ad4ea9186fbb9b57
DantheSE <dan.doyin@gmail.com>
2023-09-19 10:05:46 -0700

    first task for new alx module (pointers and arrays)

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - changes value of pointer to 98
+ *
+ * @n: value gotten from user to be dereferenced
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
