94e435b81fc9eb5ab4f3205faac24436533cd13d
DantheSE <dan.doyin@gmail.com>
2024-05-27 21:25:02 +0100

    completed challenge 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,14 +1,1 @@
-#ifndef MAIN_H
-#define MAIN_H
-/**
- * print_alphabet - function that loops effectively printing the full alphabet
- */
-int _putchar(char c);
-void print_alphabet(void)
-{
-for (print_alphabet = 'a'; print_alphabet <= 'z'; print_alphabet++)
-{
-	printf("%c", print_alphabet);
-}
-}
-#endif /* MAIN_H */
+int _putchar(char);
