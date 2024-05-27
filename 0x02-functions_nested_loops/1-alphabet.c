530c39e60fc4a5927cc512e3384897a630fba294
DantheSE <dan.doyin@gmail.com>
2024-05-27 22:03:45 +0100

    completed challenge 2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,13 +6,19 @@
  * Return: Always(0) Success
  */
 
+void print_alphabet(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+
+	_putchar('\n');
+}
+
 int main(void)
 {
-	char character;
-	for (character = 'a'; character <= 'z'; character++)
-	{
-		_putchar(character);
-	}
-	_putchar('\n');
+	print_alphabet();
 	return (0);
 }
