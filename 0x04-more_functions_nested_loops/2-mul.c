8b317d3d045f3ddc76b57b5d61815bb3330eb5dc
DantheSE <dan.doyin@gmail.com>
2024-08-24 14:33:56 +0100

    function for multiplication, passes linting checks with betty

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - takes two integers and multiplies them
+ * @a: first integer to be collected
+ * @b: second integer to be collected
+ * Return: result of multiplication of the two integers
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
