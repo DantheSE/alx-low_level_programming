e01b7da06cabe6d2e0aabba42539b2c6b2f1dc1c
DantheSE <dan.doyin@gmail.com>
2024-08-24 13:52:46 +0100

    modified pre-existing files, changed contents of header file, added putchar file which includes the putchar function

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,18 @@
-#include "main.h"
-#include <stdio.h>
-
 /**
- * main - check the code.
+ *_isupper - confirms presecnce of capital letetrs
  *
- * Return: Always 0.
+ * @c: the character to check
+ * Return: 0 or 1
  */
-int main(void)
+
+int _isupper(int c)
 {
-char c;
-c = 'A';
-printf("%c: %d\n", c, _isupper(c));
-c = 'a';
-printf("%c: %d\n", c, _isupper(c));
-return (0);
+    if (c >= 'A' && c <= 'Z')
+    {
+        return (1);
+    }
+    else
+    {
+        return (0);
+    }
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,42 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- *_isupper - confirms presecnce of capital letetrs
- *
- * @c: the character to check
- * Return: 0 or 1
- */
+int _putchar(char c);
+int _isupper(int c);
 
-
-int _isupper(int c)
-{
-	if (c >= 'A' && c <= 'Z')
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
-}
-/**
- * _isdigit - checks if a character is a digit between 0 and 9
- * @c: the character to check
- *
- * Return:
- * 1 if the character is a digit between 0 and 9, 0 otherwise.
- */
-
-int _isdigit(int c)
-{
-	if (c >= 48 && c <= 57)
-	{
-	return (1);
-	}
-	else
-	{
-	return (0);
-	}
-}
-#endif /*MAIN_H*/
+#endif
