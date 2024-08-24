3ff87670223346000dc7eaeb9144b7c315445a26
DantheSE <dan.doyin@gmail.com>
2024-08-24 14:46:12 +0100

    code to run through digits 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_numbers - prints numbers from 0 to 9
+ * Return: nothing
+ */
+
+void print_numbers(void)
+{
+int i = 0;
+
+while (i < 10)
+{
+_putchar(i);
+i++;
+}
+_putchar('\n');
+}
