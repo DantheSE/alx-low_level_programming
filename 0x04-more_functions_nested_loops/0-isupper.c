e09624d6549465a986e869d14218d9ebd6ba1f48
DantheSE <dan.doyin@gmail.com>
2024-08-24 14:06:56 +0100

    added a newline to file(pleasing betty)

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,3 +1,5 @@
+#include "main.h"
+#include <stdio.h>
 /**
  *_isupper - confirms presecnce of capital letetrs
  *
@@ -7,12 +9,12 @@
 
 int _isupper(int c)
 {
-if (c >= 'A' && c <= 'Z')
+	if (c >= 'A' && c <= 'Z')
 {
-    return (1);
+	return (1);
 }
-else
+	else
 {
-    return (0);
+	return (0);
 }
 }
