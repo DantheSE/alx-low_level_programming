ca908f14ec2fccd75194201982f15fdd65c387b5
DantheSE <dan.doyin@gmail.com>
2024-08-24 14:18:33 +0100

    created function to check for digits in input, followed betty coding style for c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isdigit - checks if a digit is present in input
+ *
+ * @c: character being collected from user
+ * Return: 1 if it's a digit or 0 if it's not
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+{
+	return (1);
+}
+	else
+{
+	return (0);
+}
+}
