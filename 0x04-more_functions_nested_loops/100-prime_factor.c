e4689801b3a7e1e24577f3f6748c6c953a885ed2
DantheSE <dan.doyin@gmail.com>
2025-03-20 10:10:45 +0100

    Finished advanced tasks

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - program to find the largest prime factor of a large number
+ * Return - returns 0 for success and 1 for failure  
+ */
+
+int main()
+{
+    unsigned long number = 612852475143;
+    unsigned long i,  largest;
+
+    if (number % 2 == 0)
+    {
+        largest = 2;
+        number /= 2; 
+    }
+
+    for (i = 3; i * i <= number; i += 2)
+    {
+        if ((number % i) == 0)
+        {
+            largest = i;
+            number /= i;
+        }
+    }
+
+    if (number > largest)
+    {
+        largest = number;
+    }
+    printf("Largest factor: %lu\n", largest);
+    return (0);
+}
