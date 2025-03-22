bf4ea24dfaee3fa8b7e0dbc58c7d84ef0e2fe670
DantheSE <dan.doyin@gmail.com>
2025-03-22 22:50:04 +0100

    Compeleted task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints an array of integers
+ * @a: address of first array
+ * @n: Number of array elements to be printed
+ * Return: Nothing
+ */
+void print_array(int *a, int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+	{
+		printf("%d, ",a[i]);
+	}
+	_putchar('\n');
+}
