78f77cfb74065e8f459cc373d619f4a5046d8d1a
DantheSE <dan.doyin@gmail.com>
2025-03-31 11:11:10 +0100

    Finished last task

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -4,6 +4,23 @@
  * @s: supplied string
  * Return: encoded string
  */
+
 char *leet(char *s)
 {
+    char letters[11] = "aAeEoOtTlL";
+    char numbers[11] = "4433007711";
+    int i, j;
+
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        for (j = 0; j < 11; j++)
+        {
+            if (s[i] == letters[j])
+            {
+                s[i] = numbers[j];
+                break;
+            }
+        }
+    }
+    return (s);
 }
