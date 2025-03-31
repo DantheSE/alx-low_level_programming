b833dcae5c4328ae91a2df6447921dcffb2c339a
DantheSE <dan.doyin@gmail.com>
2025-03-31 17:30:53 +0100

    Removed test lines in advanced task 3

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -19,8 +19,5 @@ int main(void)
   *(p - 3) = 98;
   /* ...so that this prints 98\n */
   printf("a[2] = %d\n", a[2]);
-  printf("p = %p\n", &n);
-  printf("a = %p\n", a);
-  printf("a[2] = %p\n", &a[2]);
   return (0);
 }
